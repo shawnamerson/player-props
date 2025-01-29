e95426bb54d993c93c801dac2ca0da73acc86445
shawnamerson <shawnamerson@gmail.com>
2025-01-28 18:26:18 -0600

    Created the Footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "./ui/Navbar";
+import Footer from "./ui/Footer";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,6 +18,7 @@ export default function RootLayout({
       <body className="relative">
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
